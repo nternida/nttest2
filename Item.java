7fbe3bb78c9ba71b655207832d35129e9a5ca35a
caseyhuang <casey.huang@e-hps.com>
2013-10-16 10:41:12 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,10 +1,12 @@
 public class Item {
   private String name;
+  private BigDecimal price;
   
   public Item() {
   }
   
-  public Item(String name) {
+  public Item(String name, BigDecimal price) {
     this.name = name;
+    this.price = price;
   }
 }
