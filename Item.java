13478b82b1c01b1d5d1fb8c0baed1a2fd0236a7b
Noel Ternida <noel.ternida@e-hps.com>
2013-10-17 14:02:59 -0500

    Added email to Item.

--- Item.java
+++ Item.java
@@ -1,12 +1,14 @@
 public class Item {
   private String name;
   private Long id;
+  private String emailAddress;
   
   public Item() {
   }
   
-  public Item(Long id, String name) {
+  public Item(Long id, String name, String emailAddress) {
     this.id = id;
     this.name = name;
+    this.emailAddress = emailAddress;
   }
 }
